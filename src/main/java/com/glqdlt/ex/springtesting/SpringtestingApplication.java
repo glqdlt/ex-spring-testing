f364134e92b688100c49af9cc8cffad41639512c
전일웅 <jhun@nm-4ones.com>
2018-07-03 11:37:09 +0900

    init

---
+++ src/main/java/com/glqdlt/ex/springtesting/SpringtestingApplication.java
@@ -0,0 +1,12 @@
+package com.glqdlt.ex.springtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtestingApplication.class, args);
+    }
+}
